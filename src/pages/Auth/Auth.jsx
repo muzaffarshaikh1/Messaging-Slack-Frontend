bf45f8f03a4259948b959ecc691771646402c644
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-12-25 15:59:10 +0530

    added custom color in tailwind

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Auth = () => {
   return (
-    <div className='h-[100vh] flex justify-center items-center bg-[#5c3B58]'>
+    <div className='h-[100vh] flex justify-center items-center bg-slack'>
         <div className="md:h-auto md:w-[420px]">
             <SignupCard/>
         </div>
