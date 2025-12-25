82442e57c63011c9bf4db7cdf77a6e012b69e18b
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-12-25 11:28:23 +0530

    add shadcn and eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -23,7 +24,14 @@ export default defineConfig([
       },
     },
     rules: {
+      'simple-import-sort/imports':'error',
+      'simple-import-sort/exports':'error',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'semi':['error','always'],
+      'quotes':['error','single']
     },
+    plugins:{
+      'simple-import-sort':eslintPluginSimpleImportSort
+    }
   },
 ])
