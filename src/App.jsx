4ee8b557e0f68b08b9a43f15c80b49a142f3d4ce
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-12-25 17:03:05 +0530

    added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import Auth from "./pages/Auth/Auth";
-import SigninCard from "./components/organisms/Auth/SigninCard";
-import SignupCard from "./components/organisms/Auth/SignupCard";
+import Auth from "@/pages/Auth/Auth";
+import SigninCard from "@/components/organisms/Auth/SigninCard";
+import SignupCard from "@/components/organisms/Auth/SignupCard";
+import { NotFound } from "@/components/organisms/Auth/NotFound";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/auth/signin" element={<Auth><SigninCard /></Auth>} />
         <Route path="/auth/signup" element={<Auth><SignupCard /></Auth>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </>
   )
---
+++ src/components/organisms/Auth/NotFound.jsx
@@ -0,0 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import React from 'react'
+import { useNavigate } from 'react-router'
+
+export const NotFound = () => {
+
+    const navigate = useNavigate()
+
+    return (
+        <div className="flex h-screen w-full flex-col items-center justify-center bg-grey-100">
+            <Card className="text-center shadow-lg max-w-lg">
+                <CardHeader>
+                    <CardTitle>404 Not Found</CardTitle>
+                    <p className='text-gray-600'>The page you are looking for does not exists</p>
+                </CardHeader>
+                <CardContent>
+                    <img className='rounded-lg' src='https://www.digitalmesh.com/blog/wp-content/uploads/2020/05/404-error.jpg' />
+                    <Button
+                        variant='outline'
+                        className='mt-4'
+                        onClick={()=>navigate(-1)}
+                    >
+                        Go Back
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
